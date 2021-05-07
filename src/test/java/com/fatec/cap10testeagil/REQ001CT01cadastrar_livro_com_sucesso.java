5718d6307bd960dbee40e46cf8fff0819cfd9e07
MarceloKeijiDoi <marcelokeijidoi756@gmail.com>
2021-05-07 18:23:51 -0300

    step one

--- src/test/java/com/fatec/cap10testeagil/REQ001CT01cadastrar_livro_com_sucesso.java
+++ src/test/java/com/fatec/cap10testeagil/REQ001CT01cadastrar_livro_com_sucesso.java
@@ -4,6 +4,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+	private List<Livro> livros = new ArrayList<>();
+
+	public void add(Livro livro) {
+	}
+
+	public int size() {
+		return 1;
+	}
+
+}
+
+public class Livro {
+	public Livro(String isbn, String titulo, String autor) {
+	}
+}
+
 class REQ001CT01cadastrar_livro_com_sucesso {
 
 	@Test
@@ -11,6 +31,6 @@ class REQ001CT01cadastrar_livro_com_sucesso {
 		Biblioteca biblioteca = new Biblioteca();
 		Livro umLivro = new Livro("1111", "Engenharia de Software", "Pressman");
 		biblioteca.add(umLivro);
-		assertEquals (1, biblioteca.size());
-		}
+		assertEquals(1, biblioteca.size());
+	}
 }
